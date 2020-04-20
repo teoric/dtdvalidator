e27006eeb6e4c611665e294e4a5b62930cf8b638
Bernhard Fisseni <bernhard.fisseni@mail.de>
2020-04-20 08:34:58 +0200

    modularize, make Eclipse happy

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+/**
+ * @author bernh
+ *
+ */
+module i5validator {
+    exports de.mannheim.ids.clarin.xml;
+
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires info.picocli;
+    requires java.xml;
+    requires org.apache.commons.compress;
+    requires org.apache.commons.io;
+    requires org.slf4j;
+}
